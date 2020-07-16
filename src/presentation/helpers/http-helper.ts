901c72b5f8ded771d2d48878921d40edf8351ea2
Ian Oliveira de Medeiros <ianprogrammer@gmail.com.br>
2020-07-16 10:11:00 -0300

    chore: adiciona metodos de ajuda para tratar erros

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,29 @@
+import { HttpResponse } from '../interfaces'
+import { ServerError, UnauthorizedError } from '../erros'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+
